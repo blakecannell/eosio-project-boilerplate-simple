5a7fb2b33c51dd210c28b01e7d1783273f008580
harry.soiland <harry.soiland@rocketboots.com>
2018-08-05 00:18:07 +1000

    backend almost complete

--- backend/controllers/EosController.js
+++ backend/controllers/EosController.js
@@ -1,14 +1,23 @@
-const EosService = require("../services/EosService");
-
-
+config = {
+    chainId: null, // 32 byte (64 char) hex string
+    keyProvider: ['5JXYNSdz1h9UgrRpvWQiGi21VB269BQ31CVRSNXQRMRTjY9kjRE'], // WIF string or array of keys..
+    httpEndpoint: 'Ec2-52-10-35-115.us-west-2.compute.amazonaws.com',
+    expireInSeconds: 60,
+    broadcast: true,
+    verbose: false, // API activity
+    sign: true
+};
+accountName = "johnsmith123";
+contractName = "Straasapp123";
+tableName = "proposal";
+numberOfResults = "100";
 class EosController {
 	constructor() {
 	}
 
 	async createProposal (req, res) {
-		//sort out the args here
 		try {
-			const response = await EosService.put(args);
+            const response = eos.contract(contractName).then(contractName => contractName.createprop(req.body));
 			res.send(response);
 		} catch (err) {
 			console.log.error(err);
@@ -16,16 +25,67 @@ class EosController {
 		}
 	}
 
-	async getData (req, res) {
-		//sort out the args here
+    async castVote (req, res) {
+        try {
+            const response = eos.contract(contractName).then(contractName => contractName.castvote(req.body));
+            res.send(response);
+        } catch (err) {
+            console.log.error(err);
+            res.status(500).send(err);
+        }
+    }
+
+	async getAll (req, res) {
 		try {
-			const response = await EosService.getData(args)
+			const response = eos.getTableRows(
+                {
+                    json: true,
+                    code: contractName, //eos.token
+                    scope: accountName, //not sure about this but i think it may be the account name
+                    table: tableName, //the name of the table
+                    limit: numberOfResults //maximum number of
+                }
+            ).then(
+                (result) => {
+                	return result
+                }).catch(
+                (err) => {
+                    console.log.error(err);
+                    res.status(500).send(err);
+                });
             res.send(response);
 		} catch (err) {
 			console.log.error(err);
 			res.status(500).send(err);
 		}
 	}
+
+	async getById(req, res) {
+        try {
+            const response = eos.getTableRows(
+                {
+                    json: true,
+                    code: contractName, //eos.token
+                    scope: accountName, //not sure about this but i think it may be the account name
+                    table: tableName, //the name of the table
+					lower_bound: req.body,
+                    limit: numberOfResults //maximum number of
+                }
+            ).then(
+                (result) => {
+                    return result
+                }).catch(
+                (err) => {
+                    console.log.error(err);
+                    res.status(500).send(err);
+                });
+            res.send(response);
+        } catch (err) {
+            console.log.error(err);
+            res.status(500).send(err);
+        }
+	}
+
 }
 
 module.exports = EosController;
